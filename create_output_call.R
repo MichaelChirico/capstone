d28e43e77d62de1a2ec9112b97fec70dfe5c8f7a
Michael Chirico <MichaelChirico4@gmail.com>
2017-08-10 19:18:27 -0700

    need to force string in sh call

--- create_output_call.R
+++ create_output_call.R
@@ -12,6 +12,6 @@ for (wk in sprintf('201703%02d', c(1, 8, 15, 22))) {
                     k, l1, l2, kde.bw, kde.lags, kde.win)
           ][which.max(V1), cat(
             do.call(paste, c(list('Rscript show_prediction.r'), .SD)),
-            paste0(' ', wk), '\n', sep = '', file = ff, append = TRUE
+            paste0(' "', wk, '"'), '\n', sep = '', file = ff, append = TRUE
           ), .SDcols = !'V1']
 }
